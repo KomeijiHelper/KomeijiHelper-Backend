8f8871c41fa3a08c322ac1ef7f99a9eab2993b0b
cjw2331 <3108762629@qq.com>
2025-04-03 08:17:25 +0800

    feat:聊天记录存储

--- src/main/java/komeiji/back/websocket/persistence/impl/RedisRecordstorage.java
+++ src/main/java/komeiji/back/websocket/persistence/impl/RedisRecordstorage.java
@@ -4,7 +4,10 @@ import jakarta.annotation.Resource;
 import komeiji.back.utils.RedisUtils;
 import komeiji.back.websocket.persistence.MessageRecord;
 import komeiji.back.websocket.persistence.RecordStorage;
+import komeiji.back.websocket.persistence.meta.Meta;
+import org.json.JSONObject;
 
+import java.io.*;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,6 +16,7 @@ public class RedisRecordstorage implements RecordStorage {
     private RedisUtils redisUtils;
 
     private UUID uuid;
+    private Meta meta;
 
     public void set_Uuid(UUID uuid){
         this.uuid = uuid;
@@ -34,7 +38,42 @@ public class RedisRecordstorage implements RecordStorage {
     }
 
     @Override
-    public void close() {
+    public void close()  {
+        String filePath = this.meta.getStorePath();
+        redisUtils.lpush(uuid.toString(),this.meta);
 
+        OutputStreamWriter osw = null;
+        try {
+            osw = new OutputStreamWriter(new FileOutputStream(filePath),"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        JSONObject obj=new JSONObject();//创建JSONObject对象
+        obj.put("meta",redisUtils.lpop(uuid.toString()));
+
+        long sz = redisUtils.getListSize(uuid.toString());
+        for(long i = 0;i<sz;i++)
+        {
+            JSONObject subObj=new JSONObject();//创建对象数组里的子对象
+            subObj.put(String.valueOf(i),redisUtils.lpop(uuid.toString()));
+            obj.accumulate("Message",subObj);
+        }
+
+        try {
+            osw.write(obj.toString());
+            osw.flush();//清空缓冲区，强制输出数据
+            osw.close();//关闭输出流
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void setMeta(Meta metadata) {
+       this.meta = metadata;
     }
 }
