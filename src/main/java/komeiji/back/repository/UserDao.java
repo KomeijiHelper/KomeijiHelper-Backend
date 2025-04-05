fd97ab6674a152fa3be0a3a5771d28151c2fe76e
cjw2331 <113898114+3108762629@users.noreply.github.com>
2025-04-05 11:59:41 +0800

    feat:用户信息修改 (#7)

--- src/main/java/komeiji/back/repository/UserDao.java
+++ src/main/java/komeiji/back/repository/UserDao.java
@@ -1,8 +1,12 @@
 package komeiji.back.repository;
 
+import jakarta.transaction.Transactional;
 import komeiji.back.entity.UserClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import komeiji.back.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.*;
 
 import java.util.List;
@@ -16,4 +20,9 @@ public interface UserDao extends JpaRepository<User,Long>{
     User findByUserNameAndPassword(String uname, String password);
     List<User> findAllByUserClass(UserClass userClass);
     List<User> findAll();
+
+    @Modifying
+    @Transactional
+    @Query("update User u set u.userName = ?1,u.password = ?2,u.userClass = ?3,u.email = ?4 where u.id = ?5")
+    int updateUser(@Param("1") String uname, @Param("2") String password, @Param("3") UserClass userClass, @Param("4") String email, @Param("5") long id);
 }
--- src/main/java/komeiji/back/service/UserService.java
+++ src/main/java/komeiji/back/service/UserService.java
@@ -6,9 +6,11 @@ import komeiji.back.entity.UserClass;
 import java.util.List;
 
 public interface UserService {
-    Boolean loginService(String userName ,String password);
+    User loginService(String userName ,String password);
     Boolean registerService(User user);
     User getUserByName(String uname);
     List<User> getUsersByUserClass(UserClass userClass);
     List<User> getAllUsers();
+    int updateUser(User user);
+    User getUserById(long id);
 }
